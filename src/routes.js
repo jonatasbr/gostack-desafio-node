057189f220bb9f1610d1d225f71634cf3ae9c71e
Jonatas Braz <jonatasbraz@gmail.com>
2019-09-18 20:06:07 -0300

    listar meetups do organizador - usuário logado

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import MeetupController from './app/controllers/MeetupController';
+import EventOwnerController from './app/controllers/EventOwnerController';
 
 import authMiddleware from './app/middleware/auth';
 
@@ -30,6 +31,8 @@ routes.get('/meetups', MeetupController.index);
 routes.post('/meetups', MeetupController.store);
 routes.put('/meetups/:id', MeetupController.update);
 routes.delete('/meetups/:id', MeetupController.delete);
+
+routes.get('/events', EventOwnerController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
