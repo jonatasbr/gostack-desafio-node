8a03562e337c257d6a2cdf4f5a7bda5829856ac3
Jonatas Braz <jonatasbraz@gmail.com>
2019-09-28 03:01:16 -0300

    corrigindo visualizar imagem em editar

--- src/app/controllers/EventOwnerController.js
+++ src/app/controllers/EventOwnerController.js
@@ -1,8 +1,18 @@
 import Meetup from '../models/Meetup';
+import File from '../models/File';
 
 class EventOwnerController {
   async index(req, res) {
-    const meetups = await Meetup.findAll({ where: { user_id: req.userId } });
+    const meetups = await Meetup.findAll({
+      where: { user_id: req.userId },
+      include: [
+        {
+          model: File,
+          as: 'image',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
     return res.json(meetups);
   }
 }
